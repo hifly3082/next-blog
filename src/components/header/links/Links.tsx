554220736b7794fca8faa6194db7407b3a9d7ab7
mark.avvakumov <maavvakumov@gmail.com>
2024-03-11 17:31:27 +0700

    add links / adjust spacing

---
+++ src/components/header/links/Links.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { useState } from 'react'
+import NavLink from './NavLink'
+import styles from './links.module.scss'
+import classNames from 'classnames'
+
+export interface LinkI {
+  title: string
+  path: string
+}
+
+const links = [
+  { title: 'Home', path: '/' },
+  { title: 'Works', path: '/works' },
+  { title: 'Blog', path: '/blog' },
+  { title: 'Contact', path: '/contact' }
+]
+
+const Links = () => {
+  const [open, setOpen] = useState(false)
+  // const session = true
+
+  const toggleOpen = () => {
+    setOpen((prev) => !prev)
+  }
+
+  return (
+    <>
+      {/* DESKTOP */}
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {/* {session ? (
+          <button className={styles.logoutButton}>Logout</button>
+        ) : (
+          <NavLink item={{ title: 'Login', path: '/login' }} />
+        )} */}
+      </div>
+
+      {/* MOBILE */}
+      <button
+        className={classNames(styles.menuButton, open && styles.active)}
+        onClick={toggleOpen}>
+        <span></span>
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Links

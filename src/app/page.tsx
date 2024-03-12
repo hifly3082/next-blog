4188ba7a108a6cfe60d67da8068d3743e2a44790
mark.avvakumov <maavvakumov@gmail.com>
2024-03-12 21:03:39 +0700

    Add Hero + Posts + Works sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import Hero from '@/components/hero/Hero'
+import Posts from '@/components/posts/Posts'
+import Works from '@/components/works/Works'
 import Image from 'next/image'
 // import styles from "./page.module.css";
 
 export default function Home() {
-  return <main>home</main>
+  return (
+    <main>
+      <Hero />
+      <Posts />
+      <Works />
+    </main>
+  )
 }

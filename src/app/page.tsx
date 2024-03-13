54698f354762036654588fb580e0f30dcb00fc2f
mark.avvakumov <maavvakumov@gmail.com>
2024-03-13 20:02:28 +0700

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import Hero from '@/components/hero/Hero'
 import Posts from '@/components/posts/Posts'
 import Works from '@/components/works/Works'
-import Image from 'next/image'
-// import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Hero />
       <Posts />
       <Works />
-    </main>
+    </>
   )
 }

a4e96eb655df358b23ba3e35de7bf2adc3c9a8ae
mark.avvakumov <maavvakumov@gmail.com>
2024-03-13 20:03:23 +0700

    update post layout and styles

--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -1,40 +1,43 @@
+import classNames from 'classnames'
 import styles from './posts.module.scss'
 
 const Posts = () => {
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>Recent posts</h2>
-      <article className={styles.post}>
-        <h3 className={styles.postTitle}>
-          Making a design system from scratch
-        </h3>
-        <p className={styles.postInfo}>
-          <span className={styles.postDate}>12 Feb 2020</span> |{' '}
-          <span className={styles.postTags}>Design, Pattern</span>
-        </p>
-        <p className={styles.postBody}>
-          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-          sint. Velit officia consequat duis enim velit mollit. Exercitation
-          veniam consequat sunt nostrud amet.
-        </p>
-      </article>
-      <article className={styles.post}>
-        <h3 className={styles.postTitle}>
-          Creating pixel perfect icons in Figma
-        </h3>
-        <p className={styles.postInfo}>
-          <span className={styles.postDate}>12 Feb 2020</span> |{' '}
-          <span className={styles.postTags}>Design, Pattern</span>
-        </p>
-        <p className={styles.postBody}>
-          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-          sint. Velit officia consequat duis enim velit mollit. Exercitation
-          veniam consequat sunt nostrud amet.
-        </p>
-      </article>
-      {/* <a href='/blog' className={styles.blogLink}>
-        View all
-      </a> */}
+    <div className={styles.bg_blue}>
+      <section className='container'>
+        <div className={styles.header}>
+          <h2 className='section-title'>Recent posts</h2>
+          <a href='/blog'>View all</a>
+        </div>
+        <div className='flex'>
+          <article className={styles.post}>
+            <h3 className={styles.title}>
+              Making a design system from scratch
+            </h3>
+            <p className={styles.info}>
+              <span>12 Feb 2020</span> | <span>Design, Pattern</span>
+            </p>
+            <p>
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+          </article>
+          <article className={styles.post}>
+            <h3 className={styles.title}>
+              Creating pixel perfect icons in Figma
+            </h3>
+            <p className={styles.info}>
+              <span>12 Feb 2020</span> | <span>Design, Pattern</span>
+            </p>
+            <p>
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+          </article>
+        </div>
+      </section>
     </div>
   )
 }

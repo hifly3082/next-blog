9d2c15c6d1dfd33179d12256991fb2496ef3879b
mark.avvakumov <maavvakumov@gmail.com>
2024-03-13 20:03:35 +0700

    update hero layout and styles

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,29 +3,25 @@ import styles from './hero.module.scss'
 
 const Hero = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image
-          src='/photo.png'
-          alt='hero-image'
-          fill
-          className={styles.heroImage}
-        />
+    <section className='container'>
+      <div className='flex'>
+        <div className={styles.image}>
+          <Image src='/photo.png' alt='hero-image' fill />
+        </div>
+        <div>
+          <h1>
+            Hi, I am John, <br />
+            Creative Technologist
+          </h1>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
+          <button>Download Resume</button>
+        </div>
       </div>
-      {/* <div> */}
-      <h1 className={styles.title}>
-        Hi, I am John, <br />
-        Creative <br />
-        Technologist
-      </h1>
-      <p className={styles.description}>
-        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-        sint. Velit officia consequat duis enim velit mollit. Exercitation
-        veniam consequat sunt nostrud amet.
-      </p>
-      <button className={styles.button}>Download Resume</button>
-      {/* </div> */}
-    </div>
+    </section>
   )
 }
 export default Hero

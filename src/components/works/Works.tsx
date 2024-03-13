396a1477542bd85b92e81d89992c8f18a9aab800
mark.avvakumov <maavvakumov@gmail.com>
2024-03-13 20:04:12 +0700

    update works layout and styles

--- src/components/works/Works.tsx
+++ src/components/works/Works.tsx
@@ -3,24 +3,25 @@ import styles from './works.module.scss'
 
 const Works = () => {
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>Featured works</h2>
-      <div className={styles.workCard}>
-        <div className={styles.workImg}>
+    <section className='container'>
+      <h2 className='section-title'>Featured works</h2>
+      <div className='flex'>
+        <div className={styles.image}>
           <Image src='/work1.png' alt='work1' fill />
         </div>
-        <h3 className={styles.workTitle}>Designing Dashboard</h3>
-        <div className={styles.workInfo}>
-          <span className={styles.year}>2020</span>{' '}
-          <span className={styles.tag}>Dashboard</span>
-          <p className={styles.description}>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.
-          </p>
+        <div>
+          <h3>Designing Dashboard</h3>
+          <div>
+            <span>2020</span> <span> Dashboard</span>
+            <p>
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 export default Works

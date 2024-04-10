18acacbed634441156dd1d83b8a9dd9d7deb3ee2
mark.avvakumov <maavvakumov@gmail.com>
2024-04-10 23:08:36 +0700

    remove Redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,7 @@
 import type { Metadata } from 'next'
-import dynamic from 'next/dynamic'
 import { Heebo } from 'next/font/google'
-
-import Header from '@/components/header/Header'
-import Footer from '@/components/footer/Footer'
 import styles from './layout.module.scss'
 import './globals.scss'
-
-const ReduxProvider = dynamic(() => import('@/store/provider'), {
-  ssr: false
-})
 
 const heebo = Heebo({ subsets: ['latin'] })
 
@@ -24,15 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning={true}>
       <body className={heebo.className}>
-        <ReduxProvider>
-          <div className={styles.container}>
-            <Header />
-            <main className={styles.main}>{children}</main>
-            <Footer />
-          </div>
-        </ReduxProvider>
+        <div className={styles.container}>
+          <main className={styles.main}>{children}</main>
+        </div>
       </body>
     </html>
   )

341cfd6efb70b5a11a30104718dae638653cc54f
mark.avvakumov <maavvakumov@gmail.com>
2024-04-11 18:53:27 +0700

    add auth links

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,27 @@
+import { UserButton, auth } from '@clerk/nextjs'
 import Links from './links/Links'
 import styles from './header.module.scss'
+import Link from 'next/link'
 
-const Header = () => {
+const Header = async () => {
+  const { userId } = await auth()
+  const isAuth = !!userId
+
   return (
     <header className={styles.header}>
+      {!isAuth ? (
+        <div className={styles.profile}>
+          <Link href='/login'>Log In</Link>
+          <Link href='/signup'>Sign Up</Link>
+        </div>
+      ) : (
+        <div className={styles.profile}>
+          <UserButton afterSignOutUrl='/sign-in' />
+          <Link href='/profile' className={styles.link}>
+            Profile
+          </Link>
+        </div>
+      )}
       <Links />
     </header>
   )

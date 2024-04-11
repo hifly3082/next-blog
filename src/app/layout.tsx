d491076979e67112046f1e1bd258f7cc3c30e6b2
mark.avvakumov <maavvakumov@gmail.com>
2024-04-11 18:54:43 +0700

    add ClerkProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next'
 import { Heebo } from 'next/font/google'
+import { ClerkProvider, auth } from '@clerk/nextjs'
+
+import Header from '@/components/header/Header'
+import Footer from '@/components/footer/Footer'
 import styles from './layout.module.scss'
 import './globals.scss'
 
@@ -10,18 +14,25 @@ export const metadata: Metadata = {
   description: 'John +'
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const { userId } = await auth()
+  const isAuth = !!userId
+
   return (
-    <html lang='en' suppressHydrationWarning={true}>
-      <body className={heebo.className}>
-        <div className={styles.container}>
-          <main className={styles.main}>{children}</main>
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en' suppressHydrationWarning={true}>
+        <body className={heebo.className}>
+          <div className={styles.container}>
+            {isAuth && <Header />}
+            <main className={styles.main}>{children}</main>
+            {isAuth && <Footer />}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }

052129db4a6304c7da3c8372e9d330bbcf6d0f5f
mark.avvakumov <maavvakumov@gmail.com>
2024-04-16 20:06:06 +0700

    fetch single post by Id

--- src/app/blog/[postId]/page.tsx
+++ src/app/blog/[postId]/page.tsx
@@ -1,17 +1,25 @@
 async function getPostById(postId: string) {
-  const response = await fetch(`http://localhost:3000/api/posts/${postId}`, {
-    method: 'GET'
+  const res = await fetch(`https://dummyjson.com/posts/${postId}`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
   })
 
-  return response.json()
+  if (!res.ok) {
+    throw new Error('Failed to fetch post')
+  }
+
+  const data = await res.json()
+  return data
 }
 
 const PostId = async ({ params }: { params: { postId: string } }) => {
-  const { post } = await getPostById(params.postId)
+  const post = await getPostById(params.postId)
 
   return (
-    <div>
-      <p>Post {params.postId}</p>
+    <div className='container-md'>
+      <p>Post #{params.postId}</p>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
     </div>

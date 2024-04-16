3bde3a33572697e5bcf51d37f6caf14de2a12d52
mark.avvakumov <maavvakumov@gmail.com>
2024-04-16 20:02:49 +0700

    refactor with PostThumbnail

--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -1,41 +1,38 @@
-import classNames from 'classnames'
+import PostThumbnail from './PostThumbnail'
 import styles from './posts.module.scss'
 
-const Posts = () => {
+async function getPosts() {
+  const res = await fetch('https://dummyjson.com/posts', {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch post')
+  }
+
+  const data = await res.json()
+  return data.posts.slice(0, 2)
+}
+
+const Posts = async () => {
+  const posts = await getPosts()
+
   return (
     <div className={styles.bg_blue}>
       <section className='container'>
         <div className={styles.header}>
           <h2 className='section-title'>Recent posts</h2>
-          <a href='/blog'>View all</a>
+          <a href='/blog' className={styles.link}>
+            View all
+          </a>
         </div>
         <div className='flex'>
-          <article className={styles.post}>
-            <h3 className={styles.title}>
-              Making a design system from scratch
-            </h3>
-            <p className={styles.info}>
-              <span>12 Feb 2020</span> | <span>Design, Pattern</span>
-            </p>
-            <p>
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint. Velit officia consequat duis enim velit mollit.
-              Exercitation veniam consequat sunt nostrud amet.
-            </p>
-          </article>
-          <article className={styles.post}>
-            <h3 className={styles.title}>
-              Creating pixel perfect icons in Figma
-            </h3>
-            <p className={styles.info}>
-              <span>12 Feb 2020</span> | <span>Design, Pattern</span>
-            </p>
-            <p>
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint. Velit officia consequat duis enim velit mollit.
-              Exercitation veniam consequat sunt nostrud amet.
-            </p>
-          </article>
+          {posts.map((post) => (
+            <PostThumbnail post={post} key={post.id} />
+          ))}
         </div>
       </section>
     </div>
